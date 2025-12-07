5129b3f068057182c616610a0bd8853d1ae8a299
Fawaaazi <muhammedfawas90@gmail.com>
2025-12-07 11:39:38 +0530

    refactored the script file to make it readable

--- frontend/script.js
+++ frontend/script.js
@@ -3,57 +3,78 @@
 
 // const { url } = require("inspector");
 const form = document.querySelector("form")
-let data;
 const table = $("table")
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+})
+
+function RenderAttendenceTable(data){
+    let count = 1;
+    data.forEach(Element =>{
+        RenderTableTr(Element);
+    })
+}
+
+function RenderTableTr(Element){
+    let count = 1
+    const inp = $(`<input type="number" name="${Element.name}" min="0" max="31" class="input-days" >`)
+    const tdInp = $(`<td class="day-field"></td>`)
+    const tr = $(` <tr>
+                <td>${count++}</td>
+                <td><h3>${Element.name}</h3></td>
+                </tr>`
+            )
+    tdInp.append(inp)
+    tr.append(tdInp)
+    table.append(tr) 
+    inputPreventDefaultSubmitAndSwitching(inp)
+}
+
+function inputPreventDefaultSubmitAndSwitching(inp){
+    inp.on("keydown", function(e){
+        console.log("on")
+        
+        const inputs = $("input").toArray()
+        let index = inputs.indexOf(this)
+            if(e.key === "Enter"){
+            e.preventDefault()
+            if(inputs[index +1]){
+                inputs[index +1].focus()
+            }
+
+        }
+
+    })     
+}
+
 $.ajax({
-    url:"https://mess-app-1qlz.onrender.com",
+    url:"https://mess-app-1qlz.onrender.com/",
     method: "GET",
     success:(res)=>{
-        data = res
-        console.log(data)
-        let count = 1;
-        data.forEach(Element =>{
-            const inp = $(`<input type="number" name="${Element.name}" min="0" max="31" class="input-days" >`)
-            inp.on("keydown", function(e){
-                // console.log("on")
-                
-                const inputs = $("input").toArray()
-                let index = inputs.indexOf(this)
-                 if(e.key === "Enter"){
-                    e.preventDefault()
-                   if(inputs[index +1]){
-                        inputs[index +1].focus()
-                   }
-
-                }
-
-            })     
-            const tdInp = $(`<td class="day-field"></td>`)
-            const tr = $(` <tr>
-                        <td>${count++}</td>
-                        <td><h3>${Element.name}</h3></td>
-                        </tr>`
-                    )
-            tdInp.append(inp)
-            tr.append(tdInp)
-            table.append(tr) 
-        })
-                   
+        // console.log(data)    
+        RenderAttendenceTable(res)   
     },
     error:(err)=>{
         console.log(err)
     }
 
 })
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()
-})
-$("#submit-btn").on("click",(e)=>{
-        if(!form.checkValidity()){
-            form.reportValidity()
-            return;
-        }
-        const inputsArray = document.getElementsByClassName("input-days");
+
+
+
+
+function checkFormValidity(){
+    if(!form.checkValidity()){
+        form.reportValidity()
+        return false;
+    }
+    return true
+
+}
+
+function getDaysPresent(){
+    const inputsArray = document.getElementsByClassName("input-days");
         console.log(inputsArray)
         let daysPresent = [];
         for( i =0; i < inputsArray.length; i++){
@@ -63,86 +84,119 @@ $("#submit-btn").on("click",(e)=>{
             })
 
         }
+        return daysPresent;
+}
 
-        console.log(daysPresent)
-        $.ajax({
-           url:"https://mess-app-1qlz.onrender.com/present",
-           method: "POST",
-           data: JSON.stringify(daysPresent),
-           contentType: "application/json",
-           dataType: 'json',
-           success : (res)=>{
-                
-                $("form").html("")
-                $("form").append(`<h2> Total Number of Points ${res.totalDays}</h2>
-                <div id="expense-container">
-                    <div id="expense-div">
-                        <label for="messExpense">Total Mess Expense</label><br>
-                        <input type="Number" min="0" name="messExpense" id="messExpense">
-                    </div>
-                        <div class="mess-alavance-div">
-                        <label for="alavance">Mess Alavance</label><br>
-                        <input type="number" name="Alavance" id="alavance">
-                    </div>
-                    <input type="submit" value="Submit" id="expense-submit-btn">
-                </div>
-                `)  
-                $("#expense-submit-btn").on("click",(e)=>{
-                        e.preventDefault()
-                        if(!form.checkValidity()){
-                            form.reportValidity()
-                            return
-                        }
-                       let expense = {
-                            totalExpense : Number.parseInt($("#messExpense").val()),
-                            alavance : Number.parseInt($("#alavance").val())
-                        }
-                        $.ajax({
-                            url:"https://mess-app-1qlz.onrender.com/expense",
-                            method: "POST",
-                            data: JSON.stringify(expense),
-                            contentType: "application/json",
-                            dataType: 'json',
-                            success: (res)=>{
-                                console.log(res)
-                                $(".main").html(`<table border="1" id="mess-bill-table">
-                                        <thead>
-                                            <th>SI No</th>
-                                            <th>Name</th>
-                                            <th>No of Days Present</th>
-                                            <th>Per Day Expense</th>
-                                            <th>Alavance</th>
-                                            <th>Mess Bill</th>
-                                            <th>Total</th>
-                                        </thead>
-                                        <tbody>
-                                        </tbody>
-                                    </table>`)
-                                let count = 1;
-                                res.forEach(Element =>{
-                                    const tr = $(`<tr>
-                                        <td><h3>${count++}</h3></td>
-                                        <td><h3>${Element.name}</h3></td>
-                                        <td><h3>${Element.present}</h3></td>
-                                         <td><h3>${Element.expensePerDay}</h3></td>
-                                        <td><h3>${Element.alavance}</h3></td>
-                                        <td><h3>${(Element.messExpense).toFixed(2)}</h3></td>
-                                        <td><h3 style="color:red;">${(Element.messExpense + Element.alavance).toFixed(2)}</h3></td>
-                                    </tr>`)
-                                    $("#mess-bill-table tbody").append(tr)
-                                })
-                                
-                            },
-                            error: (err)=>{
-                                console.log(err)
-                            }
-                        })
-                })      
+function RenderExpenseForm(res){
+    $("form").html("")
+    $("form").append(`<h2> Total Number of Points ${res.totalDays}</h2>
+        <div id="expense-container">
+            <div id="expense-div">
+                <label for="messExpense">Total Mess Expense</label><br>
+                <input type="Number" min="0" name="messExpense" id="messExpense">
+            </div>
+                <div class="mess-alavance-div">
+                <label for="alavance">Mess Alavance</label><br>
+                <input type="number" name="Alavance" id="alavance">
+            </div>
+            <input type="submit" value="Submit" id="expense-submit-btn">
+        </div>
+   `)  
+}
 
-           },
-           error: (err)=>{
-                console.log(err)
-           }
-        })
+$("#submit-btn").on("click",async (e)=>{
+        if(!checkFormValidity()){
+            return;
+        }
+        let daysPresent = getDaysPresent()
+        
+        try{
+            console.log(daysPresent)
 
+            const response1 = await $.ajax({
+                url:"https://mess-app-1qlz.onrender.com/present",
+                method: "POST",
+                data: JSON.stringify(daysPresent),
+                contentType: "application/json",
+                dataType: 'json',
+            }) 
+            
+            RenderExpenseForm(response1)
+
+              
+        }
+        catch (err){
+            console.log(err)
+        }
 })
+
+
+
+function getExpense(){
+    let expense = {
+        totalExpense : Number.parseInt($("#messExpense").val()),
+        alavance : Number.parseInt($("#alavance").val())
+    }
+    return expense
+}
+
+function renderMessBillTable(res){
+
+     $(".main").html(`
+        <table border="1" id="mess-bill-table">
+            <thead>
+                <th>SI No</th>
+                <th>Name</th>
+                <th>No of Days Present</th>
+                <th>Per Day Expense</th>
+                <th>Alavance</th>
+                <th>Mess Bill</th>
+                <th>Total</th>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>`
+    )
+    renderMessBillTableTr(res)
+}
+
+function renderMessBillTableTr(res){
+    let count = 1;
+    res.forEach(Element =>{
+        const tr = $(`
+            <tr>
+                <td><h3>${count++}</h3></td>
+                <td><h3>${Element.name}</h3></td>
+                <td><h3>${Element.present}</h3></td>
+                <td><h3>${Element.expensePerDay}</h3></td>
+                <td><h3>${Element.alavance}</h3></td>
+                <td><h3>${(Element.messExpense).toFixed(2)}</h3></td>
+                <td><h3 style="color:red;">${(Element.messExpense + Element.alavance).toFixed(2)}</h3></td>
+            </tr>`
+    )
+        $("#mess-bill-table tbody").append(tr)
+    })
+}
+
+$("form").on("click","#expense-submit-btn",async (e)=>{
+            e.preventDefault()
+            if(!checkFormValidity()){
+                return
+            }
+            try{
+                let expense = getExpense()
+                let response2 = await $.ajax({
+                    url:"https://mess-app-1qlz.onrender.com/expense",
+                    method: "POST",
+                    data: JSON.stringify(expense),
+                    contentType: "application/json",
+                    dataType: 'json'
+                })
+
+                renderMessBillTable(response2)
+            }
+            catch(err){
+                console.log(err)
+            }
+})
+                       
